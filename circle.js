64f52f2252d4f78f7fc50e914f996d17280dbff7
claire <cdersky@gmail.com>
2016-05-07 15:15:02 -0700

    initial commit

---
+++ circle.js
@@ -0,0 +1,8 @@
+console.log('Claire\'s NPM package was loaded...');
+
+module.exports = {
+  hypnotize : function() {
+   console.log('Nicolas Cage. niChoLas caGe. NICOLAS CAGEEEEEEE!!! RAGE!!!!!!');
+ }
+};
+
---
+++ index.js
@@ -0,0 +1,5 @@
+const circle = require('./circle');
+
+console.log('invoke hypnotize, if you dareee');
+console.log('hypnotize()');
+circle.hypnotize();
